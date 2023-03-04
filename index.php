f6ef68a66a5f9992381b81e22b2fafd9d054abb4
NARooshnavand <nasserakhoundi@gmail.com>
2023-03-04 03:17:51 -0800

    Table was completed books was shown in table and bootstrap was added as CDN

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Lesson 1</title>
 </head>
 <body>
@@ -30,12 +31,22 @@
     ];
     ?>
     <h3>Books list</h3>
-    <table>
-        <?php foreach($books as $key=>$value){ ?>
+    <table class="table table-bordered table-dark">
+        <thead>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Publish Date</th>
+            <th>Price</th>
+        </thead>
+        <tbody>
+        <?php foreach($books as $book){ ?>
             <tr>
-                <td> <?= $key ?> </td> <td> <?php var_dump($value) ?> </td>    
+                <?php foreach($book as $key=>$value){ ?>
+                    <td><?= $value ?></td>
+                <?php } ?>    
             </tr>    
         <?php } ?>
+        </tbody>
     </table>
 </body>
 </html>
