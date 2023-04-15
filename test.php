7dfe8e2122becb00b63eba4f14c4d64e0f641268
NARooshnavand <nasserakhoundi@gmail.com>
2023-04-15 05:06:48 -0700

    completed of class 26-01-1402

--- test.php
+++ test.php
@@ -2,8 +2,11 @@
 include 'functions.php';
 
 require('Database.php');
-$db = new Database();
-$books = $db->query("SELECT * FROM books WHERE title LIKE '%book%'")
-            ->fetchAll(PDO::FETCH_ASSOC);
-dd($books);
+$config = require('Config.php');
+$db = new Database($config);
+$id = $_GET['id'];
+$query = "SELECT * FROM users WHERE id= :id";
+$user = $db->query($query,['id'=>$id])
+            ->fetch(PDO::FETCH_ASSOC);
+dd($user);
 ?>
